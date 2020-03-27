94311885475efaeb44355a1ac5564ce8794d1170
kinga sadowska <kingaasadowska@gmail.com>
2020-03-27 15:21:59 +0100

    Add images

--- src/data/chefs.js
+++ src/data/chefs.js
@@ -1,22 +1,27 @@
 export const chefs = [
     {
-        name: 'Wolfgang Puck',
+        image: 'https://hips.hearstapps.com/del.h-cdn.co/assets/cm/15/10/54f637e864f9c_-_puck-chef-lg.jpg',
+        name: 'Wolfgang Johannes Puck',
         description: 'Wolfgang Johannes Puck is an Austrian-American chef, restaurateur, and actor. Born in Austria, Puck moved to the United States at the age of 24. In 1973, Puck moved to Los Angeles, opening his first restaurant, Spago, in 1982.',
         link: 'https://www.wolfgangpuckcookingschool.com/recipes/wolfgangs-signature-recipes/',
     },
     {
-        name: 'Jamie Oliver',
+        image: 'https://cdn.jamieoliver.com/library/images/Jamie-Social.jpg',
+        name: 'James Trevor Oliver',
         description: 'James Trevor Oliver MBE is a British chef and restaurateur. He is known for his approachable cuisine, which has led him to front numerous television shows and open many restaurants.',
         link: 'https://www.jamieoliver.com/recipes/',
     },
     {
+        image: 'https://www.irishtimes.com/polopoly_fs/1.4113705.1576179370!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg',
         name: 'Marco Pierre White',
         description: 'Marco Pierre White is a British chef, restaurateur, and television personality. White has been dubbed the first celebrity chef, and the enfant terrible of the UK restaurant scene. In 1994, at age 32, White became both the youngest chef and the first British chef to be awarded three Michelin stars.',
         link: 'https://www.marcopierrewhite.co/latest-news/tags/Recipes',
     },
     {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ1yda3BpA9YP_pkgKlpmE3h56ZqnraJr47LG1-TT9CKuYizLBt',
         name: 'Gordon James Ramsay',
         description: 'Gordon James Ramsay OBE is a British chef, restaurateur, writer, television personality and food critic. He was born in Johnstone, Scotland, and raised in Stratford-upon-Avon, England. His restaurants have been awarded 16 Michelin stars in total and currently hold a total of seven.',
         link: 'https://www.gordonramsay.com/gr/recipes/',
     },
+    
 ]
